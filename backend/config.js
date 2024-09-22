72d534a95b20f7104a1b7efa875268355cfb53c6
imisgsc <simigracia@gmail.com>
2024-09-22 19:04:42 +0530

    added mongoose to nodejs

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 
+    'mongodb+srv://simigracia:root@mern-book-store.5ggar.mongodb.net/books-collection?retryWrites=true&w=majority&appName=MERN-BOOK-STORE'
